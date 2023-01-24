0ee605bb2c6aad3558e53eb749d3a63a87921e2e
Ishwor Shrestha <ishwor.shretsha@apexcollege.edu.np>
2023-01-24 07:44:35 +0545

    Added Dependecy Injection

--- ApexRestaurant.Repository/RepositoryModule.cs
+++ ApexRestaurant.Repository/RepositoryModule.cs
@@ -6,12 +6,16 @@ namespace ApexRestaurant.Repository
 {
     public static class RepositoryModule
     {
-        public static void Register(IServiceCollection services, string connection,
-        string migrationsAssembly)
+        public static void Register(IServiceCollection services)
         {
-            services.AddDbContext<RestaurantContext>(options =>
-        options.UseSqlServer(connection, builder =>
-        builder.MigrationsAssembly(migrationsAssembly)));
+
+            string migrationsAssembly = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+
+            services.AddDbContext<RestaurantContext>(options => options.UseSqlServer(
+                @"Server=DESKTOP-13L30S5\SQLEXPRESS;Initial Catalog=samaj_db;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False",
+                builder => builder.MigrationsAssembly(migrationsAssembly)
+            ));
+
             services.AddTransient<ICustomerRepository, CustomerRepository>();
         }
     }
