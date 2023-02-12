e695c7172b48834a16b1ed8981f9e3042c747633
Ishwor Shrestha <ishwor.shretsha@apexcollege.edu.np>
2023-02-12 19:01:02 +0545

    Performed Database Migration

--- ApexRestaurant.Api/Program.cs
+++ ApexRestaurant.Api/Program.cs
@@ -1,17 +1,61 @@
-namespace ApexRestaurant.Api
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+// for Startup.cs file
+//namespace ApexRestaurant.Api
+// {
+//     public class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//             CreateHostBuilder(args).Build().Run();
+//         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+//         public static IHostBuilder CreateHostBuilder(string[] args) =>
+//             Host.CreateDefaultBuilder(args)
+//                 .ConfigureWebHostDefaults(webBuilder =>
+//                 {
+//                     webBuilder.UseStartup<Startup>();
+//                 });
+//     }
+// }
+
+
+using ApexRestaurant.Repository;
+using ApexRestaurant.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+/* RepositoryModule.Register(builder.Services,
+    builder.Configuration.GetConnectionString("DefaultConnection"),  "ApexRestaurant.Api"); */
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+if (string.IsNullOrEmpty(connectionString))
+{
+    Console.WriteLine("Connection String not found in appsettings.json");
+    return;
 }
+RepositoryModule.Register(builder.Services, connectionString, "ApexRestaurant.Api");
+
+ServicesModule.Register(builder.Services);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
