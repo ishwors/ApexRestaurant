e7d1892a6c9e59cc1adead2d0b5225ed92f56fee
Ishwor Shrestha <ishwor.shretsha@apexcollege.edu.np>
2023-02-12 13:34:37 +0545

    added services for all

--- ApexRestaurant.Services/ServiceModule.cs
+++ ApexRestaurant.Services/ServiceModule.cs
@@ -1,5 +1,11 @@
 using Microsoft.Extensions.DependencyInjection;
 using ApexRestaurant.Services.SCustomer;
+using ApexRestaurant.Services.SMeal;
+using ApexRestaurant.Services.SMealDish;
+using ApexRestaurant.Services.SMenu;
+using ApexRestaurant.Services.SMenuItem;
+using ApexRestaurant.Services.SStaff;
+using ApexRestaurant.Services.SStaffRole;
 
 namespace ApexRestaurant.Services
 {
@@ -8,6 +14,12 @@ namespace ApexRestaurant.Services
         public static void Register(IServiceCollection services)
         {
             services.AddTransient<ICustomerService, CustomerService>();
+            services.AddTransient<IMealService, MealService>();
+            services.AddTransient<IMealDishService, MealDishService>();
+            services.AddTransient<IMenuService, MenuService>();
+            services.AddTransient<IMenuItemService, MenuItemService>();
+            services.AddTransient<IStaffService, StaffService>();
+            services.AddTransient<IStaffRoleService, StaffRoleService>();
         }
     }
 }
